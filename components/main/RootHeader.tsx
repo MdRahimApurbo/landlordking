4c927027be67d3acf9a5ee49bc3e64a4f99c59ec
Md Rahim Apurbo <engr.mdapurbo@gmail.com>
2024-11-05 00:54:46 +0600

    add volumn

--- components/main/RootHeader.tsx
+++ components/main/RootHeader.tsx
@@ -4,10 +4,12 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Slider,
+  SliderValue,
   Switch,
   User,
 } from "@nextui-org/react";
-import { MoonIcon, SunIcon, Volume2 } from "lucide-react";
+import { MoonIcon, SunIcon, Volume, Volume2 } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { UserCard } from "../card/usercard";
@@ -15,6 +17,7 @@ import { UserCard } from "../card/usercard";
 const RootHeader = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [value, setValue] = useState<SliderValue>(25);
 
   useEffect(() => {
     setMounted(true);
@@ -25,7 +28,54 @@ const RootHeader = () => {
     <div className="container">
       <div className="flex justify-between items-center py-10 px-40 sticky top-0 ">
         <div className="flex gap-5 items-center">
-          <Button size="sm" isIconOnly startContent={<Volume2 />} />
+          <Popover placement="bottom-start" showArrow={true}>
+            <PopoverTrigger>
+              <Button size="sm" isIconOnly startContent={<Volume2 />} />
+            </PopoverTrigger>
+            <PopoverContent className="w-[500px] bg-primary-700">
+              <div className="flex flex-row gap-2 w-full h-full max-w-md items-center justify-center">
+                <Slider
+                  aria-label="Volume"
+                  size="md"
+                  color="primary"
+                  value={value}
+                  onChange={setValue}
+                  startContent={
+                    <Button
+                      isIconOnly
+                      variant="light"
+                      radius="full"
+                      onPress={() =>
+                        setValue((prev) =>
+                          Number(prev) >= 10 ? Number(prev) - 10 : 0
+                        )
+                      }
+                    >
+                      <Volume className="text-2xl" />
+                    </Button>
+                  }
+                  endContent={
+                    <Button
+                      isIconOnly
+                      variant="light"
+                      radius="full"
+                      onPress={() =>
+                        setValue((prev) =>
+                          Number(prev) <= 90 ? Number(prev) + 10 : 100
+                        )
+                      }
+                    >
+                      <Volume2 className="text-2xl" />
+                    </Button>
+                  }
+                  className="max-w-md"
+                />
+                <p className="text-default-500 font-medium text-small w-6">
+                  {value}
+                </p>
+              </div>
+            </PopoverContent>
+          </Popover>
           <Switch
             defaultSelected
             size="lg"
